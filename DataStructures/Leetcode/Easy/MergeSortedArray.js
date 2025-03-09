68cb721bc420b0eb6e256e415f1e590864adb263
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-03-08 20:37:21 -0500

    Data structures: Updated Merge sorted array

--- DataStructures/Leetcode/Easy/MergeSortedArray.js
+++ DataStructures/Leetcode/Easy/MergeSortedArray.js
@@ -1,43 +1,46 @@
-/** Leetcode 88, Blind 150
- * Merges two sorted arrays nums1 and nums2 into nums1 as one sorted array.
- * Assumes that nums1 has enough space (size m + n) to hold additional elements from nums2.
+
+/**
+ * 88. Merge Sorted Array, Blind 150
+ *
+ * Merges two sorted arrays in-place.
+ * The merged result is stored in nums1.
+ *
+ * Time Complexity: O(m + n)
+ * Space Complexity: O(1)
  *
  * @param {number[]} nums1 - First sorted array with extra space at the end.
  * @param {number} m - Number of valid elements in nums1.
  * @param {number[]} nums2 - Second sorted array.
  * @param {number} n - Number of elements in nums2.
- * @return {void} Do not return anything; modify nums1 in-place.
+ * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    // mn: pointer for the last index of the merged array in nums1.
-    // i: pointer for the last valid element in nums1.
-    // j: pointer for the last element in nums2.
+    // Pointer for the last index of the merged array.
     let mn = m + n - 1;
+    // Pointer for the last valid element in nums1.
     let i = m - 1;
+    // Pointer for the last element in nums2.
     let j = n - 1;
 
-    // Merge the arrays from the end to the beginning.
-    // This prevents overwriting elements in nums1 that haven't been compared yet.
+    // Merge the arrays from the end until one of them is exhausted.
     while (i >= 0 && j >= 0) {
-        // Compare current elements from nums1 and nums2.
+        // Compare elements from the end of nums1 and nums2.
         if (nums1[i] >= nums2[j]) {
-            // Place the larger element at the current end of nums1.
+            // If the element in nums1 is larger, place it at the current merge position.
             nums1[mn] = nums1[i];
-            i--;  // Move pointer in nums1.
+            i--;
         } else {
-            // Place the element from nums2.
+            // Otherwise, place the element from nums2.
             nums1[mn] = nums2[j];
-            j--;  // Move pointer in nums2.
+            j--;
         }
-        mn--;  // Move merged pointer to the left.
+        mn--;
     }
 
-    // If there are any remaining elements in nums2,
-    // copy them into nums1. No need to do anything for nums1's remaining elements,
-    // because they are already in the correct place.
+    // If there are remaining elements in nums2, copy them.
     while (j >= 0) {
         nums1[mn] = nums2[j];
-        mn--;
         j--;
+        mn--;
     }
 };
