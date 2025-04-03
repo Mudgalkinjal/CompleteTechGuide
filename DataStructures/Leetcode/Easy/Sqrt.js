516bb9241227f8565bd5675655171ab151cb679f
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-04-02 20:44:58 -0400

    Data structures Added Sqrt

---
+++ DataStructures/Leetcode/Easy/Sqrt.js
@@ -0,0 +1,48 @@
+/* 69. Sqrt(x)
+
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+*/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let start = 0;
+    let end = x;
+    let ans = 0;
+
+    while(start<=end){
+        let mid = Math.floor((start+end)/2);
+        let sq = mid*mid;
+        if(sq>x){
+            end = mid-1;
+        }
+        else{
+            ans = mid;
+            start = start+1;
+        }
+    }
+    return ans
+};
