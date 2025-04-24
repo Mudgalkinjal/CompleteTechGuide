7c8ea25e5136f502f620f7aa0c4b19e843d20370
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-04-24 13:33:03 -0400

    flatten array without recursion

--- UIComponents/Javascript/flattenArr.js
+++ UIComponents/Javascript/flattenArr.js
@@ -11,3 +11,24 @@ function flattenArr(arr) {
 
 }
 const ans = flattenArr(inpArr)
+
+//Flatten a deeply nested array without recursion
+
+let inpArr2 = [1,[2,3,4],[5,6,[7,8]]];
+
+function flattenArr2(arr) {
+  let res = [];
+  let stack = [...arr];
+  while(stack.length){
+    let curr = stack.pop();
+    if(Array.isArray(curr)){
+      stack.push(...curr);
+    }
+    else{
+      res.unshift(curr)
+    }
+  }
+  return res
+}
+const ans2 = flattenArr2(inpArr2)
+console.log(ans)
